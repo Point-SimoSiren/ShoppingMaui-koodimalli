75c20dbf1cbe4acd7d11dbb45d8744dbb62eff91
Pont-SimoSiren <sirensimo7@gmail.com>
2024-11-30 19:51:28 +0200

    Add project files.

---
+++ ShoppingMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShoppingMaui/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using ShoppingBackend.Models;
+using System.Collections.ObjectModel;
+
+
+namespace ShoppingMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadDataFromRestAPI();
+        }
+
+        // LISTAN HAKEMINEN BACKENDISTÄ
+        async void LoadDataFromRestAPI()
+        {
+            // Latausilmoitus näkyviin
+            Loading_label.IsVisible = true;
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://shoppingbackendope.azurewebsites.net");
+            string json = await client.GetStringAsync("/api/shoplist/");
+
+            // Deserialisoidaan json muodosta C# muotoon
+            IEnumerable<Shoplist> ienumShoplist = JsonConvert.DeserializeObject<Shoplist[]>(json);
+
+            ObservableCollection<Shoplist> Shoplist = new ObservableCollection<Shoplist>(ienumShoplist);
+
+            itemList.ItemsSource = Shoplist;
+
+            // Latausilmoitus piiloon
+            Loading_label.IsVisible = false;
+            //addPageBtn.IsVisible = true;
+            //kerätty_nappi.IsVisible = true;
+
+        }
+
+
+
+    }
+
+}
